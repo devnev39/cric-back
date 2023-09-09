bd8914ccadeaaad769e091151504801419039e70
Dhruv Bonde <devnev39@gmail.com>
2023-09-09 18:10:11 +0530

    patches:new auction

--- src/routes/auctionRouter/teams.js
+++ src/routes/auctionRouter/teams.js
@@ -1,8 +1,9 @@
-const { addTeam, updateTeam, deleteTeam } = require("../../services/teams");
+const { addTeam, updateTeam, deleteTeam, getTeam } = require("../../services/teams");
 const { resultwrapper } = require("../../utils");
 
 module.exports = {
     addTeam : async (req,res) => await resultwrapper(addTeam,req,res,req),
     updateTeam : async (req,res) => await resultwrapper(updateTeam,req,res,req),
-    deleteTeam : async (req,res) => await resultwrapper(deleteTeam,req,res,req)
+    deleteTeam : async (req,res) => await resultwrapper(deleteTeam,req,res,req),
+    getTeam : async (req,res) => await resultwrapper(getTeam, req, res,req)
 }

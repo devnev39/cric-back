776f6d517a5cfc037fedf579ddaf9e012e27a0f2
Dhruv Bonde <devnev39@gmail.com>
2023-09-09 11:41:37 +0530

    patches:new auction

--- src/utils/auctionAdminAuth.js
+++ src/utils/auctionAdminAuth.js
@@ -2,8 +2,15 @@ require("dotenv").config();
 const { decrypt } = require(".")
 const bcrypt = require("bcrypt");
 
-module.exports.newAuctionAdminAuth = async (auctionJson) => {
-    const key = decrypt.decrypt(auctionJson.Adminid);
-    const result = await bcrypt.compare(key, process.env.ADMINID)
-    return result;
+module.exports = { 
+    newAuctionAdminAuth: async (auctionJson) => {
+        const key = decrypt.decrypt(auctionJson.Adminid);
+        const result = await bcrypt.compare(key, process.env.ADMINID)
+        return result;
+    },
+    deleteAuctionAuth: async (deleteId) => {
+        const key = decrypt.decrypt(deleteId);
+        const result = await bcrypt.compare(key, process.env.DELETEID);
+        return result;
+    } 
 }

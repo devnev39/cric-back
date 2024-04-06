c3cb6e1532f891347c312fe800e47352b6a50281
Dhruv Bonde <devnev39@gmail.com>
2024-04-06 12:08:34 +0530

    chore: default params auction

--- src/services/auction.js
+++ src/services/auction.js
@@ -62,7 +62,7 @@ module.exports = {
       auctionJson.maxPlayers = 11;
       auctionJson.allowPublicTeamView = true;
       auctionJson.allowLogin = true;
-      auctionJson.allowRealtimeUpdates = true;
+      auctionJson.allowRealtimeUpdates = false;
       auctionJson.password = await bcrypt.hash(
           decrypt.decrypt(auctionJson.password),
           5,
